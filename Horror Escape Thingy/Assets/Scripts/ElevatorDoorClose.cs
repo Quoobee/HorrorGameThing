ea9cd1be9e1d5f888835187d3cd7786141e34292
Quoobee <teddy.o.davies@gmail.com>
2018-07-04 18:19:44 -0400

    Making Progress in WaitForSeconds
    trying to make the door open up properly. This is Wednesday night

---
+++ Horror Escape Thingy/Assets/Scripts/ElevatorDoorClose.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ElevatorDoorClose : MonoBehaviour
+{
+    public bool doorOpen = false;
+    //int speed = 1;   //I removed speed, because it makes more sense, and saves space just dividing distance
+    //int distance = 200; //I removed this to simplify the code for now
+    public bool buttonPressed = false;
+    public GameObject door;
+    
+    void Start ()
+    {
+        //door.SetActive(true);   //I turned this off because we are making it move, rather than dissapear
+    }
+	
+	void Update ()
+    {
+        if (doorOpen == false && buttonPressed == true)
+        {
+            buttonPressed = false;
+            Debug.Log("Condition is met, button is set to false");
+            StartCoroutine(MoveDoor()); //This is needed in order to call an IEnumerator
+        }
+    }
+
+    IEnumerator MoveDoor() //This is needed to call WaitForSeconds
+    {
+        Debug.Log("Called MoveDoor");
+        for (int i = 0; i < 60; i++)
+        {
+            door.transform.Translate(Vector3.back );
+            Debug.Log("Moved Door");
+            yield return new WaitForSeconds(1); //this CAN NOT be in Update()
+        }
+        Debug.Log("Door is done moving, door Open is set to true");
+        doorOpen = true;
+
+
+    }
+}
---
+++ Horror Escape Thingy/Assets/Scripts/StartingElevator.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartingElevator : MonoBehaviour {
+    int distance = 10;
+    int speed = 5;
+    float decent = 0.01f;
+    
+    Vector3 myPosition;
+
+    // Use this for initialization
+    void Start () {
+        myPosition = new Vector3(10.39693f, 15.73292f, -2.612766f);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        for (int i = 0; i < distance; i++)
+        {
+            transform.position = new Vector3(transform.position.x, transform.position.y - decent, transform.position.z);
+        }
+    }
+}
